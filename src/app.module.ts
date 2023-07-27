df05c0666e45e66a57abd05712be69902c4b6286
suyong choi <dpfprtus@gmail.com>
2023-07-27 16:07:00 +0900

    socket.io 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ChatsGateway } from './chats/chats.gateway';
+import { ChatsModule } from './chats/chats.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, //다른 모듈에서도 환경변수 사용 가능
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    ChatsModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [ChatsGateway],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  private readonly isDev: boolean = process.env.MODE === 'dev' ? true : false;
+  configure() {
+    mongoose.set('debug', this.isDev); //개발모드일 때 디버그
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'public')); //css,js 바인딩 -> __dirname(현재 디렉토리)에서 상위폴더로 가서 public폴더를 찾는다.
+  app.setBaseViewsDir(join(__dirname, '..', 'views')); //view 바인딩
+  app.setViewEngine('hbs');
+
   await app.listen(3000);
 }
 bootstrap();
